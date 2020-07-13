e420cc379e9fbe5cffcab2a1054743b636cb3111
medhavi <medhavi.srivastava16@gmail.com>
2020-07-13 18:03:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/googlelogin/ShowDetails.java
@@ -0,0 +1,36 @@
+package com.example.googlelogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class ShowDetails extends AppCompatActivity {
+    GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_details);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account!=null)
+        {
+            String name=account.getDisplayName();
+            String email=account.getEmail();
+            Uri url=account.getPhotoUrl();
+            String id=account.getId();
+            System.out.println(name+" "+email+" "+id);
+
+
+        }
+    }
+}

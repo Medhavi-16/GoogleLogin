6be9076ae5bcbb18efb04d88bf37e9eb1a3256db
theanandankit <ankitanand2909@gmail.com>
2020-07-14 14:15:55 +0530

    update

--- app/src/main/java/com/example/googlelogin/ShowDetails.java
+++ app/src/main/java/com/example/googlelogin/ShowDetails.java
@@ -1,22 +1,40 @@
 package com.example.googlelogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 
 public class ShowDetails extends AppCompatActivity {
     GoogleSignInClient mGoogleSignInClient;
+    TextView name,email,id;
+    ImageView imageView;
+    Button out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_details);
+        name=findViewById(R.id.name);
+        email=findViewById(R.id.email);
+        id=findViewById(R.id.id);
+        imageView=findViewById(R.id.image);
+        out=findViewById(R.id.out);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
@@ -24,13 +42,24 @@ public class ShowDetails extends AppCompatActivity {
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         if(account!=null)
         {
-            String name=account.getDisplayName();
-            String email=account.getEmail();
+            name.setText(account.getDisplayName());
+            email.setText(account.getEmail());
             Uri url=account.getPhotoUrl();
-            String id=account.getId();
+            id.setText(account.getId());
+            Glide.with(this).load(url).into(imageView);
             System.out.println(name+" "+email+" "+id);
-
-
         }
+
+        out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        finish();
+                    }
+                });
+            }
+        });
     }
 }
